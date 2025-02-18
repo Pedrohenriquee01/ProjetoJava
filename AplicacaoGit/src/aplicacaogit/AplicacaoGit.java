18e5da98cf5e527de8ce052470fee57512e72ae7
Pedro H <2300543@fortecpraiagrande.com.br>
2025-02-18 08:09:57 -0300

    Segundo Commit

--- AplicacaoGit/src/aplicacaogit/AplicacaoGit.java
+++ AplicacaoGit/src/aplicacaogit/AplicacaoGit.java
@@ -1,12 +1,16 @@
 package aplicacaogit;
-
+import java.util.Scanner;
 public class AplicacaoGit {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Minha primeiro software"
                 + " versionado com Git, hospedado no github!");
-        
-        
+        System.out.println("Digite o priemrio valor");
+        int numero1 =scan.nextInt();
+        System.out.println("Digite o segundo valor");
+        int numero2 = scan.nextInt();
+        System.out.println("A soma é" + (numero1 + numero2));
     }
     
 }
