555e4cc571d26a4b30bb61b71803da1371929d56
Pedro H <2300543@fortecpraiagrande.com.br>
2025-02-18 07:57:07 -0300

    Primeiro Commit

---
+++ AplicacaoGit/src/aplicacaogit/AplicacaoGit.java
@@ -0,0 +1,12 @@
+package aplicacaogit;
+
+public class AplicacaoGit {
+
+    public static void main(String[] args) {
+        System.out.println("Minha primeiro software"
+                + " versionado com Git, hospedado no github!");
+        
+        
+    }
+    
+}
